96deabcc33826beaa4ceabc00ed24ccf679c61af
Heiko Mueller <heiko.muller@gmail.com>
2020-05-09 12:41:43 -0400

    Generate signatures for domains on the fly

--- src/main/java/org/opendata/curation/d4/domain/LocalDomainEdgeWriter.java
+++ src/main/java/org/opendata/curation/d4/domain/LocalDomainEdgeWriter.java
@@ -20,13 +20,9 @@ package org.opendata.curation.d4.domain;
 import java.io.File;
 import java.io.PrintWriter;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.opendata.core.constraint.GreaterThanConstraint;
@@ -38,17 +34,14 @@ import org.opendata.curation.d4.column.ExpandedColumn;
 import org.opendata.curation.d4.column.ExpandedColumnIndex;
 import org.opendata.curation.d4.column.ExpandedColumnReader;
 import org.opendata.curation.d4.signature.SignatureBlocksConsumer;
-import org.opendata.curation.d4.signature.SignatureBlocksReader;
-import org.opendata.curation.d4.signature.SignatureBlocksStream;
 import org.opendata.curation.d4.signature.trim.TrimmerType;
 import org.opendata.core.io.FileSystem;
 import org.opendata.core.io.SynchronizedWriter;
 import org.opendata.core.prune.MaxDropFinder;
 import org.opendata.core.set.HashIDSet;
-import org.opendata.core.util.MemUsagePrinter;
 import org.opendata.curation.d4.domain.graph.HexEdgeWriter;
 import org.opendata.curation.d4.signature.SignatureBlocks;
-import org.opendata.curation.d4.signature.SignatureSimilarityFilter;
+import org.opendata.curation.d4.signature.SignatureBlocksGenerator;
 import org.opendata.curation.d4.signature.trim.CentristTrimmer;
 import org.opendata.curation.d4.signature.trim.ConservativeTrimmer;
 import org.opendata.curation.d4.signature.trim.LiberalTrimmer;
@@ -65,109 +58,62 @@ import org.opendata.db.eq.EQIndex;
  */
 public class LocalDomainEdgeWriter {
                    
-    private class BufferedWorker implements SignatureBlocksConsumer {
+    public class ColumnDispatcher implements SignatureBlocksConsumer {
 
-        private final List<SignatureBlocks> _buffer;
-        private final int _bufferSize;
-        private final List<SignatureBlocksConsumer> _columns;
-        private final int _threads;
+        private final HashMap<Integer, SignatureBlocksConsumer> _columns;
+        private final EQIndex _eqIndex;
 
-        // Statistics
-        private int _readCount = 0;
-        
-        public BufferedWorker(
-                List<SignatureBlocksConsumer> columns,
-                int bufferSize,
-                int threads
-        ) {
-            _columns = columns;
-            _bufferSize = bufferSize;
-            _threads = threads;
-            
-            _buffer = new ArrayList<>(bufferSize);
+        public ColumnDispatcher(EQIndex eqIndex) {
+
+            _eqIndex = eqIndex;
+            _columns = new HashMap<>();
         }
-        
+
+        public void add(int columnId, SignatureBlocksConsumer consumer) {
+
+            _columns.put(columnId, consumer);
+        }
+
         @Override
         public void close() {
 
-            if (!_buffer.isEmpty()) {
-                this.processBuffer();
+            for (SignatureBlocksConsumer consumer : _columns.values()) {
+                consumer.close();
             }
-        }
+       }
 
         @Override
         public void consume(SignatureBlocks sig) {
 
-            _buffer.add(sig);
-            _readCount++;
-            if (_buffer.size() == _bufferSize) {
-                this.processBuffer();
+            for (int columnId : _eqIndex.get(sig.id()).columns()) {
+                if (_columns.containsKey(columnId)) {
+                    _columns.get(columnId).consume(sig);
+                }
             }
         }
 
         @Override
         public void open() {
 
+            for (SignatureBlocksConsumer consumer : _columns.values()) {
+                consumer.open();
+            }
         }
         
-        private void processBuffer() {
-        
-            System.out.println("PROCESS BUFFER OF " + _buffer.size() + " SIGNATURES (" + _readCount + ") @ " + new Date());
-            ConcurrentLinkedQueue<SignatureBlocks> queue;
-            queue = new ConcurrentLinkedQueue<>(_buffer);
-            new MemUsagePrinter().print();
-            ExecutorService es = Executors.newCachedThreadPool();
-            for (int iThread = 0; iThread < _threads; iThread++) {
-                es.execute(new EdgeWriterTask(_columns, queue));
-            }
-            es.shutdown();
-            try {
-                es.awaitTermination(_threads, TimeUnit.DAYS);
-            } catch (java.lang.InterruptedException ex) {
-                throw new RuntimeException(ex);
-            }
+        public int size() {
             
-            System.out.println("DONE BUFFER PROCESSING @ " + new Date());
-        
-            _buffer.clear();
-        }
-    }
-
-    
-    private class EdgeWriterTask implements Runnable {
-
-        private final List<SignatureBlocksConsumer> _columns;
-        private final ConcurrentLinkedQueue<SignatureBlocks> _signatures;
-        
-        public EdgeWriterTask(
-                List<SignatureBlocksConsumer> columns,
-                ConcurrentLinkedQueue<SignatureBlocks> signatures
-       ) {
-            _columns = columns;
-            _signatures = signatures;
-        }
-        
-        @Override
-        public void run() {
-
-            SignatureBlocks sig;
-            while ((sig = _signatures.poll()) != null) {
-                for (SignatureBlocksConsumer column : _columns) {
-                    column.consume(sig);
-                }
-            }
+            return _columns.size();
         }
     }
     
     public void run(
             EQIndex eqIndex,
             ExpandedColumnIndex columnIndex,
-            SignatureBlocksStream signatures,
             String trimmer,
             Threshold sigsim,
             int threads,
             SynchronizedWriter out
-    ) throws java.io.IOException {
+    ) throws java.lang.InterruptedException, java.io.IOException {
 
         Date start = new Date();
         System.out.println("START @ " + start);
@@ -185,8 +131,9 @@ public class LocalDomainEdgeWriter {
         }
         
         int[] nodeSizes = eqIndex.nodeSizes();
-
-        List<SignatureBlocksConsumer> workers = new ArrayList<>();
+        
+        HashIDSet nodes = new HashIDSet();
+        ColumnDispatcher columns = new ColumnDispatcher(eqIndex);
         for (ExpandedColumn column : columnIndex.columns()) {
             SignatureBlocksConsumer consumer;
             consumer = new HexEdgeWriter(column, out);
@@ -208,17 +155,16 @@ public class LocalDomainEdgeWriter {
                 default:
                     consumer = new NonTrimmer(column.nodes(), consumer);
             }
-            consumer.open();
-            workers.add(consumer);
+            columns.add(column.id(), consumer);
+            nodes.add(column.nodes());
         }
 
-        SignatureBlocksConsumer sigConsumer;
-        sigConsumer = new BufferedWorker(workers, 50000, threads);
-        if (!sigsim.isSatisfied(BigDecimal.ZERO)) {
-            sigConsumer = new SignatureSimilarityFilter(sigsim, sigConsumer);
-        }
+        System.out.println(columns.size() + " COLUMN GROUPS WITH " + nodes.length() + " NODES");
         
-        signatures.stream(sigConsumer);
+        ConcurrentLinkedQueue<Integer> queue;
+        queue = new ConcurrentLinkedQueue<>(nodes.toList());
+        
+        new SignatureBlocksGenerator().runWithMaxDrop(eqIndex, queue, false, true, threads, columns);
 
         Date end = new Date();
         System.out.println("END @ " + end);
@@ -244,7 +190,6 @@ public class LocalDomainEdgeWriter {
             "  --" + ARG_TRIMMER + "=<signature-trimmer> [default: " +  
                     TrimmerType.CENTRIST.toString() +":GT0.1]\n" +
             "  <eq-file>\n" +
-            "  <signature-file(s)>\n" +
             "  <columns-file>\n" +
             "  <output-file>";
     
@@ -260,11 +205,10 @@ public class LocalDomainEdgeWriter {
             System.exit(-1);
         }
         
-        Arguments params = new Arguments(ARGS, args, 4);
+        Arguments params = new Arguments(ARGS, args, 3);
         File eqFile = new File(params.fixedArg(0));
-        File signatureFile = new File(params.fixedArg(1));
-        File columnFile = new File(params.fixedArg(2));
-        File outputFile = new File(params.fixedArg(3));
+        File columnFile = new File(params.fixedArg(1));
+        File outputFile = new File(params.fixedArg(2));
 
         String trimmer = params
                 .getAsString(ARG_TRIMMER, TrimmerType.CENTRIST.toString() + ":GT0.1");
@@ -292,13 +236,12 @@ public class LocalDomainEdgeWriter {
             new LocalDomainEdgeWriter().run(
                     nodeIndex,
                     columnIndex,
-                    new SignatureBlocksReader(signatureFile),
                     trimmer,
                     sigsim,
                     threads,
                     new SynchronizedWriter(out)
             );
-        } catch (java.io.IOException ex) {
+        } catch (java.lang.InterruptedException | java.io.IOException ex) {
             LOGGER.log(Level.SEVERE, "RUN", ex);
             System.exit(-1);
         }
